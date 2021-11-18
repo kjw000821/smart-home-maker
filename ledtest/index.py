853d9a1b0b5852e132e299dc78b153cf7853af24
sinwoovj <80377193+sinwoovj@users.noreply.github.com>
2021-11-18 10:26:37 +0900

    ㅈㄱㅊㅇ

---
+++ ledtest/index.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+from flask import render_template
+import RPi.GPIO as GPIO
+
+app = Flask(__name__)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/led/on")
+def led_on():
+    try:
+        print("===check1===")
+        GPIO.output(8, GPIO.HIGH)
+        return "ok"
+    except:
+        print("===check2===")
+        return "fail"
+
+@app.route("/led/off")
+def led_off():
+    try:
+        GPIO.output(8, GPIO.LOW)
+        return "ok"
+    except:
+        return "fail"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
